6c1b82a80b476091fd5ba39d8ae1c097ee8d5e30
Dennismwangi <denniskaranjaonline@gmail.com>
2018-06-04 20:58:37 +0300

    adding js code, printing lyrics on console

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+    $ (document).ready(function() {
+        $("form").submit (function (event){
+            event.preventDefault();
+            $("textarea#display").empty();
+
+            var artiste = $("input#artiste").val();
+            var title = $("input#title").val(); 
+
+            var url = `https://api.lyrics.ovh/v1/${artiste}/${title}`;
+
+            $.getJSON(url)
+            .done(function (data){
+                
+            /* console.log(data["lyrics"]); */
+            
+            
+                <p>${data["lyrics"]}</p>
+
+
+
+
+            });
+
+
+        });
+
+
+    });
