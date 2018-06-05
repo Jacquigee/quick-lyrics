ccaf2bbbec5432ac1041f0da63846b7e1203f0a2
Dennismwangi <denniskaranjaonline@gmail.com>
2018-06-05 19:18:43 +0300

    styling paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
-    $ (document).ready(function() {
+    $(document).ready(function() {
         $("form").submit (function (event){
             event.preventDefault();
-            $("textarea#display").empty();
+            $("p#display").empty();
 
             var artiste = $("input#artiste").val();
             var title = $("input#title").val(); 
 
-            var url = `https://api.lyrics.ovh/v1/${artiste}/${title}`;
+           var url = `https://api.lyrics.ovh/v1/${artiste}/${title}`;
 
             $.getJSON(url)
-            .done(function (data){
+            .done(function(data){
+                 /*data.forEach(display =>{
+                    $("p#display").append(`<p>${data["lyrics"]}</p>`) 
+                    
+                } );*/
                 
-            console.log(data["lyrics"]);
-            
-            
-                /*<p>${data["lyrics"]}</p>*/
+           console.log(data["lyrics"]);
 
-
-
+        
 
             });
 
-
         });
-
 
     });
